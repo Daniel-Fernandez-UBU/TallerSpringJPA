53bff5240b604fad95aa4f7ea3cbef69177e0d8a
Daniel <dfb1001@alu.ubu.es>
2024-06-12 20:13:58 +0200

    Se incluye toda la parte de usuario

--- TallerJPA/src/main/java/es/ubu/lsi/TallerJPA/Model/User.java
+++ TallerJPA/src/main/java/es/ubu/lsi/TallerJPA/Model/User.java
@@ -23,5 +23,94 @@ public class User {
     @Id
     @Column(name = "email", length = 100)
     private String email;
+    
+    /** The password. */
+    @Column(name = "password", length = 100)
+    private String password;
+    
+    /** The firstname. */
+    @Column(name = "firstname", length = 100)
+    private String firstname;
+    
+    /** The lastname. */
+    @Column(name = "lastname", length = 100)
+    private String lastname;
+
+    
+    
+	/**
+	 * Gets the firstname.
+	 *
+	 * @return the firstname
+	 */
+	public String getFirstname() {
+		return firstname;
+	}
+
+	/**
+	 * Sets the firstname.
+	 *
+	 * @param firstname the new firstname
+	 */
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	/**
+	 * Gets the lastname.
+	 *
+	 * @return the lastname
+	 */
+	public String getLastname() {
+		return lastname;
+	}
+
+	/**
+	 * Sets the lastname.
+	 *
+	 * @param lastname the new lastname
+	 */
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	/**
+	 * Gets the email.
+	 *
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+
+	/**
+	 * Sets the email.
+	 *
+	 * @param email the new email
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	/**
+	 * Gets the password.
+	 *
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+
+	/**
+	 * Sets the password.
+	 *
+	 * @param password the new password
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+    
+    
+    
 
 }
--- TallerJPA/src/main/java/es/ubu/lsi/TallerJPA/Services/databaseService.java
+++ TallerJPA/src/main/java/es/ubu/lsi/TallerJPA/Services/databaseService.java
@@ -1,6 +1,13 @@
 package es.ubu.lsi.TallerJPA.Services;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import es.ubu.lsi.TallerJPA.Model.User;
+import es.ubu.lsi.TallerJPA.Repository.UserRepository;
 
 /**
  * Clase databaseService.
@@ -14,5 +21,84 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class databaseService {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	
+	/**
+	 * Método checkRegistered.
+	 * 
+	 * @param email the email
+	 * @param password the password
+	 * @return true o false
+	 */
+	public boolean checkRegistered(String email, String password) {
+		Optional<User> userOpt;
+		User user = new User();
+		
+		if (userRepository.existsById(email)){
+			userOpt = userRepository.findById(email);
+			if (userOpt.isPresent()) {
+				user = userOpt.get();
+				
+				return user.getPassword().equals(password);
+			}
+			
+		}
+		return false;		
+		
+	} 
+	
+	/**
+	 * Metodo updateProfile.
+	 * @param email the email
+	 * @param password the password
+	 */
+	public void updateProfile(String email, String password, String firstname, String lastname) {
+		
+		User user = new User();
+		
+		user.setEmail(email);
+		user.setPassword(password);
+		user.setFirstname(firstname);
+		user.setLastname(lastname);
+		
+		userRepository.save(user);
+		
+	}
+	
+	/**
+	 * Método getUser.
+	 * @param email the email
+	 * @return the user
+	 */
+	public User getUser(String email) {
+		Optional<User> userOpt;
+		User user = new User();
+		
+		if (userRepository.existsById(email)){
+			userOpt = userRepository.findById(email);
+			if (userOpt.isPresent()) {
+				user = userOpt.get();
+			}
+		}
+		return user;
+	}
+	
+	/**
+	 * Método getAllUsers.
+	 * @return lista de usuarios
+	 */
+	public List<User> getAllUsers(){
+		List<User> listUsers = (List<User>) userRepository.findAll();
+		
+		for (User user : listUsers) {
+			System.out.println(user.getEmail().toString());
+		}
+		return listUsers;
+	}
+	
+	
 
 }
